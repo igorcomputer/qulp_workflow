2767b9613771df387d223fbb536919dbd53fb7c0
igorcomputer <igorcomputer@mail.ru>
2017-06-01 18:27:11 +0400

    Gulp (LESS,PUG,LiveReload) Ready Workflow

--- README.txt
+++ README.txt
@@ -2,19 +2,21 @@
 Старт проекта NPM:
 ==================================================
 npm init          // Инициализация проекта (создание package.json)
-npm install       // Установка пакетов, прописанных в файле package.json (dependencies, devDependencies) 
+npm install       // Установка пакетов, прописанных в файле package.json (dependencies, devDependencies)
 
 Установка модулей NPM:
 ==================================================
-Лакально  - в папку node_modules в текущий каталог 
+Лакально  - в папку node_modules в текущий каталог
 Глобально - в папку node_modules в папку пользователя
 
 npm i <имя модуля> -g             // Глобально
-npm i <имя модуля>                // Лакально без записи в package.json 
-npm i <имя модуля> --save         // Локально с записью в package.json (dependencies) 
-npm i <имя модуля> --S            // --- сокращенная запись   
-npm i <имя модуля> --save-dev	  // Локально с записью в package.json (devDependencies)
-npm i <имя модуля> --D            // --- сокращенная запись   
+npm i <имя модуля>                // Лакально без записи в package.json
+npm i <имя модуля> --save         // Локально с записью в package.json 
+(dependencies)
+npm i <имя модуля> --S            // --- сокращенная запись
+npm i <имя модуля> --save-dev	    // Локально с записью в package.json 
+(devDependencies)
+npm i <имя модуля> --D            // --- сокращенная запись
 
 Команды Gulp (http://gulpjs.com):
 ==================================================
@@ -29,37 +31,37 @@ gulp --help
 Команды Bower (https://bower.io):
 ==================================================
 
-npm install bower -g    // Установка (Глобально) 
+npm install bower -g    // Установка (Глобально)
 
-bower init              // Интерактивное создание конфига bower.json  
+bower init              // Интерактивное создание конфига bower.json
 
-bower -v                // Версия 
+bower -v                // Версия
 bower search jquery     // Поиск
-bower s jquery			// Поиск (сокращенно) 
+bower s jquery			// Поиск (сокращенно)
 
 bower info jquery       // Детальная информация о библиотеке
 
-Установка библиотек: (по умолчанию в папку bower_components текущего каталога) 
+Установка библиотек: (по умолчанию в папку bower_components текущего каталога)
 --------------------------------------------------
 
-// Синтаксис: 
-bower install <package>  
-bower i <package>        // Сокращенный синтаксис 
+// Синтаксис:
+bower install <package>
+bower i <package>        // Сокращенный синтаксис
 
-// Ключи (чтобы отметить зависимости в bower.json) 
+// Ключи (чтобы отметить зависимости в bower.json)
 -S, --save
 -D, --save-dev
 
-// Примеры установки библиотек 
-bower install jquery#1.6.4 --save // определенной версии  
+// Примеры установки библиотек
+bower install jquery#1.6.4 --save // определенной версии
 bower i normalize.css
 bower install https://code.jquery.com/jquery-3.2.1.min.js
 
-bower list                    // Просмотр списка установленных библиотек 
-bower list --path             // Получение ссылок на уже скаченные библиотеки 
+bower list                    // Просмотр списка установленных библиотек
+bower list --path             // Получение ссылок на уже скаченные библиотеки
 
-bower uninstall jquery-3.2.1 --save // Удаление библиотеки (отмечаем запись в bower.json) 
-bower list                    // Проверим что удалили (выведем список) 
+bower uninstall jquery-3.2.1 --save // Удаление библиотеки (отмечаем запись в bower.json)
+bower list                    // Проверим что удалили (выведем список)
 
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,86 @@
 "use strict"
 
-var gulp         = require('gulp');
-var concatCSS    = require('gulp-concat-css'); 
-var cleanCSS     = require('gulp-clean-css'); // to minify  
-var rename       = require("gulp-rename"); 
-var notify       = require("gulp-notify"); 
-var autoprefixer = require('gulp-autoprefixer');
-var connect      = require('gulp-connect'); 
+var gulp           = require('gulp');
 
+// Server/Lifereload
+var connect        = require('gulp-connect');
+
+// Less/CSS
+var autoprefixer   = require('gulp-autoprefixer'); // for CSS only
+var concatCSS      = require('gulp-concat-css');
+var cleanCSS       = require('gulp-clean-css'); // minify
 var less           = require('gulp-less');
 var LessAutoprefix = require('less-plugin-autoprefix');
 var autoprefix     = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
-// SERVER 
+// Pug
+var pug            = require('gulp-pug');
+
+// Utils
+var rename         = require("gulp-rename");
+var notify         = require("gulp-notify");
+
+// -------- Tasks ----------
+
+// SERVER
 gulp.task('connect', function() {
 	connect.server({
 		root: 'public_html',
 		livereload: true
 	});
-}); 
-
-// LESS to CSS 
-gulp.task('less', function () { 
-  return gulp.src('./src/less/main_styles.less') 
-    .pipe(less({
-    	plugins: [autoprefix] 
-    })) 
-    .pipe(gulp.dest('./public_html/css')) 
-    .pipe(connect.reload()); 
 });
 
-//CSS minify (start 'less' before css_minify)  
-gulp.task('css_minify', ['less'], function(){  
-	return gulp.src('./public_html/css/main_styles.css') // src files  
-		.pipe(cleanCSS())                                // minify 
-		.pipe(rename('main_styles.min.css'))             // rename after minify
-		.pipe(gulp.dest('./public_html/css/'))           // save to destination dir 
-		.pipe(connect.reload()); 
-		//.pipe(notify('--- CSS Done OK! --'));
-}); 
+// LESS to CSS
+gulp.task('less', function () {
+  return gulp.src('./src/less/main_styles.less')
+    .pipe(less({
+    	plugins: [autoprefix]
+    }))
+    .on('error', notify.onError(function (error) {
+			return 'Error Compiling LESS:.\n'+ error.message;
+		}))
+    .pipe(gulp.dest('./public_html/css'))
+    .pipe(connect.reload());
+});
 
-gulp.task('html', function(){
-	return gulp.src('./public_html/*.html') 
+//CSS minify (start 'less' before css_minify)
+gulp.task('css_minify', ['less'], function(){
+	return gulp.src('./public_html/css/main_styles.css') // src files
+		.pipe(cleanCSS())                                // minify
+		.pipe(rename('main_styles.min.css'))             // rename after minify
+		.pipe(gulp.dest('./public_html/css/'))           // save to destination dir
+		.pipe(connect.reload());
+		//.pipe(notify('--- CSS Done OK! --'));
+});
+
+gulp.task('pug', function buildHTML(){
+	return gulp.src([
+		'./src/pug/index.pug',
+		'./src/pug/page_1.pug'
+		])
+		.pipe(pug({pretty:true}))
+		.on('error', notify.onError(function (error) {
+			return 'Error Compiling PUG:.\n'+ error.message;
+		}))
+		.pipe(gulp.dest('./public_html/'))
 		.pipe(connect.reload());
 });
 
 gulp.task('watch', function(){
 	gulp.watch('./src/less/*.less', ['css_minify']) // less with minify css
-	gulp.watch('./public_html/*.html', ['html']) 
+	gulp.watch('./src/pug/**/*.pug', ['pug'])
 });
 
-gulp.task('default', ['connect', 'css_minify', 'html', 'watch']); // compile less with minify css 
+gulp.task('default', ['connect', 'css_minify', 'pug', 'watch']); // compile less with minify css
 
 /*
 
 CONSOLE:
-$ gulp 
+$ gulp
 
 Browser (livereload):
-http://localhost:8080  
+http://localhost:8080
 http://localhost:8080/index.html
 http://localhost:8080/about.html
 
-*/  
+*/
